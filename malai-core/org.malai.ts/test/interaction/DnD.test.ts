d3d8750f6f7ce8e202aab963f4fffd1daf74f167
digwendal <gwendal.didot@irisa.fr>
2018-06-05 10:40:45 +0200

    new(ts): implement of DragAndDrop integration

---
+++ malai-core/org.malai.ts/test/interaction/DnD.test.ts
@@ -0,0 +1,38 @@
+/*
+ * This file is part of Malai.
+ * Copyright (c) 2009-2018 Arnaud BLOUIN Gwendal DIDOT
+ * Malai is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later version.
+ * Malai is distributed without any warranty; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+
+import {FSMHandler} from "../../src-core/fsm/FSMHandler";
+import {StubFSMHandler} from "../fsm/StubFSMHandler";
+import {EventRegistrationToken} from "../../src/interaction/Events";
+import {createMouseEvent} from "./StubEvents";
+import {DnD} from "../../src/interaction/library/DnD";
+
+jest.mock("../fsm/StubFSMHandler");
+
+let interaction: DnD;
+let canvas: HTMLElement;
+let handler: FSMHandler;
+
+beforeEach(() => {
+    jest.clearAllMocks();
+    jest.useFakeTimers();
+    handler = new StubFSMHandler();
+    interaction = new DnD(false, false);
+    interaction.log(true);
+    interaction.getFsm().log(true);
+    interaction.getFsm().addHandler(handler);
+    document.documentElement.innerHTML = "<html><div><canvas id='canvas1' /></div></html>";
+    const elt = document.getElementById("canvas1");
+    if (elt !== null) {
+        canvas = elt;
+    }
+});
+
