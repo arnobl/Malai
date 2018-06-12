0931cd826933feb4ad0511fcb4bc47e7725c12a6
digwendal <gwendal.didot@irisa.fr>
2018-06-12 09:04:42 +0200

    (new)(ts): Setup of the App look

--- malai-example/ts-FSMEditor/src/app/app.module.ts
+++ malai-example/ts-FSMEditor/src/app/app.module.ts
@@ -3,13 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './Component/title/title.component';
-import { NewButtonComponent } from './Component/new-button/new-button.component';
+import { NewButtonComponent } from './Component/menu-button/new-button/new-button.component';
+import { SaveButtonComponent } from './Component/menu-button/save-button/save-button.component';
+import {MenuButtonComponent} from './Component/menu-button/menu-button.component';
+import { FSMpartSelectorComponent } from './Component/fsmpart-selector/fsmpart-selector.component';
+import { FSMpartComponent } from './Component/fsmpart-selector/fsmpart/fsmpart.component';
+import { XmlViewComponent } from './View/xml-view/xml-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleComponent,
-    NewButtonComponent
+    NewButtonComponent,
+    SaveButtonComponent,
+    MenuButtonComponent,
+    FSMpartSelectorComponent,
+    FSMpartComponent,
+    XmlViewComponent
   ],
   imports: [
     BrowserModule
