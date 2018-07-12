d920d332a74b1da22d65a94a64700ab9210cb9bf
digwendal <gwendal.didot@irisa.fr>
2018-07-12 09:15:27 +0200

    new(ts): Adding a way to dynamically bind new sub-element of an element specified by onContent().

--- malai-core/org.malai.ts/src/binding/KeyNodeBinder.ts
+++ malai-core/org.malai.ts/src/binding/KeyNodeBinder.ts
@@ -32,6 +32,7 @@ export class KeyNodeBinder<C extends CommandImpl> extends KeyBinder<C, KeyNodeBi
             () => {},
             () => {},
             this.widgets,
+            this.additionalWidget,
             this._async,
             false,
             this.logLevels);
